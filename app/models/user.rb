32f5593c07818b8ae22308c3164d1eadbf94596a
LulianoM <martinslucianofigueira@gmail.com>
2021-08-18 15:23:29 -0300

    topicos até seccao 6.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     validates_presence_of :email, :full_name, :location, :password
     validates_confirmation_of :password
+    validates_length_of :bio, :minimum => 30, :allow_blank => false
+    validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 end
